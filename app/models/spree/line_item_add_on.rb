7dcd3eb3fc55971eb276517a1f040735ef2192a8
Dylan Johnston <dylan@freerunningtechnologies.com>
2013-09-03 16:06:37 -0700

    .expires?
    Change-Id: Ib079a8a70d339b9c000e35d5ee49d7ccc03ccf40

--- app/models/spree/line_item_add_on.rb
+++ app/models/spree/line_item_add_on.rb
@@ -1,4 +1,10 @@
 class Spree::LineItemAddOn < ActiveRecord::Base
   belongs_to :add_on
   belongs_to :line_item
+
+  def expired?
+    return false unless add_on.expiration_days
+
+    (self.created_at + add_on.expiration_days.days).past?
+  end
 end
--- spec/models/spree/line_item_add_on_spec.rb
+++ spec/models/spree/line_item_add_on_spec.rb
@@ -3,4 +3,32 @@ require 'spec_helper'
 describe Spree::LineItemAddOn do
   it { should belong_to :add_on }
   it { should belong_to :line_item }
+
+  describe '#expired?' do
+    let(:add_on) { stub_model Spree::AddOn, expiration_days: expiration_date }
+    let(:line_item_addon) { stub_model Spree::LineItemAddOn, add_on: add_on, created_at: created_at }
+
+    subject { line_item_addon }
+
+    context "without an expiration date" do
+      let(:created_at) { 1.day.ago }
+      let(:expiration_date) { nil }
+
+      it { should_not be_expired }
+    end
+
+    context "with an expiration date" do
+      let(:expiration_date) { 5 }
+
+      context "a one day old line_item addon" do
+        let(:created_at) { 1.day.ago }
+        it { should_not be_expired }
+      end
+
+      context "a six day old line_item addon" do
+        let(:created_at) { 6.days.ago }
+        it { should be_expired }
+      end
+    end
+  end
 end
